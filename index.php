1d7e0ec220a0494c954321803fde510e41f9da7b
kholidsalam <kholidmsy@gmail.com>
2026-02-27 09:00:10 +0800

    mengubah nama

--- index.php
+++ index.php
@@ -42,9 +42,9 @@ if (!isset($_SESSION["user"])) {
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-book-open"></i>
+                    <i class="fas fa-laptop"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">Perpus Digital<sup>15</sup></div>
+                <div class="sidebar-brand-text mx-3">Iventori Lab <sup>15</sup></div>
             </a>
 
             <!-- Divider -->
